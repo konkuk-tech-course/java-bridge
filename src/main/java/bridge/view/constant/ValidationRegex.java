04068cb77bbacdbd9244da700ff175576a5a7a9b
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 14:31:52 +0900

    feat(ValidationRegex): 재시도 입력 유효성 검증 위한 정규표현식 추가

--- src/main/java/bridge/view/constant/ValidationRegex.java
+++ src/main/java/bridge/view/constant/ValidationRegex.java
@@ -3,7 +3,8 @@ package bridge.view.constant;
 public enum ValidationRegex {
     EMPTY("^$"),
     ONLY_NUMBER("^[0-9]+$"),
-    MOVE_OPTION("^[UD]{1}$");
+    MOVE_OPTION("^[UD]{1}$"),
+    RETRY_OPTION("^["+ RetryOptions.RETRY.get() + RetryOptions.QUIT.get() + "]{1}$");
 
     private final String regex;
 
