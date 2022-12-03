0f7f5b4aef24698e6c3ac2e8d26f93e62cafcee5
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 14:53:48 +0900

    feat(OutputView): printMap 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,12 @@
 package bridge.view;
 
+import bridge.service.constant.ChoiceResult;
+import bridge.view.constant.BridgeStyle;
+import bridge.view.constant.ChoiceResultStyle;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -17,7 +24,25 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<List<ChoiceResult>> bridge) {
+        bridge.stream()
+                .map(this::styleBridgePanels)
+                .map(this::styleBridge)
+                .forEach(System.out::println);
+    }
+
+    private List<String> styleBridgePanels(List<ChoiceResult> bridgeLine) {
+        return bridgeLine.stream()
+                .map(ChoiceResultStyle::findStyleByChoiceResult)
+                .collect(Collectors.toList());
+    }
+
+    private String styleBridge(List<String> bridgeLine) {
+        return bridgeLine.stream().collect(Collectors.joining(
+                BridgeStyle.DELIMITER.get(),
+                BridgeStyle.START.get(),
+                BridgeStyle.END.get()
+        ));
     }
 
     /**
