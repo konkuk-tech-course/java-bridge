4928f1e86bd506d651db564d1ec0173be3476932
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 10:49:45 +0900

    feat(DomainErrorMessage): 다리 길이 유효성 에러메세지 추가

---
+++ src/main/java/bridge/domain/constant/DomainErrorMessage.java
@@ -0,0 +1,17 @@
+package bridge.domain.constant;
+
+public enum DomainErrorMessage {
+    INVALID_RANGE_LENGTH("다리의 길이는 " + BridgeProperties.MIN_LENGTH.get() + "이상 "
+            + BridgeProperties.MAX_LENGTH.get() + "이하만 가능합니다.");
+
+    private static final String PREFIX = "[ERROR] ";
+    private final String message;
+
+    private DomainErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String get() {
+        return message;
+    }
+}
