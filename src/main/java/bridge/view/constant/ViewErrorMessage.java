8428f3a283cbbef9827fdd0bc3813f7f42bb580d
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 14:33:12 +0900

    feat(ViewErrorMessage): 올바르지 않은 재시도입력에 대한 에러메세지 추가

--- src/main/java/bridge/view/constant/ViewErrorMessage.java
+++ src/main/java/bridge/view/constant/ViewErrorMessage.java
@@ -4,7 +4,9 @@ public enum ViewErrorMessage {
     EMPTY("값을 입력하지 않았습니다."),
     NOT_NUMBER("숫자만 입력해주세요."),
     INVALID_MOVE_OPTION(MoveOptions.UP.get() + "(위), "
-            + MoveOptions.DOWN.get() + "(아래) 중 하나만 입력해주세요.");
+            + MoveOptions.DOWN.get() + "(아래) 중 하나만 입력해주세요."),
+    INVALID_RETRY_OPTION(RetryOptions.RETRY.get() + "(재시도), "
+            + RetryOptions.QUIT.get() + "(종료) 중 하나만 입력해주세요.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
