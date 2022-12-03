89b0f41b504c4a2bafc1327ac7be921ff2c6fdf9
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 15:15:53 +0900

    feat(GameController): 다리 생성 컨트롤러 기능 구현

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,13 +1,36 @@
 package bridge.controller;
 
+import bridge.domain.Bridge;
+import bridge.service.BridgeService;
+import bridge.view.InputView;
 import bridge.view.OutputView;
 
+import java.util.function.Supplier;
+
 public class GameController {
+    private static BridgeService bridgeService = BridgeService.getInstance();
+    private static InputView inputView = InputView.getInstance();
+    private static OutputView outputView = OutputView.getInstance();
+
     private static class InstanceHolder {
         private static final GameController INSTANCE = new GameController();
     }
 
     public static GameController getInstance() {
         return GameController.InstanceHolder.INSTANCE;
+    }
+
+    public Bridge createBridge() {
+        int lengthInput = requestNumberInput(inputView::readBridgeSize);
+        return bridgeService.createBridge(lengthInput);
+    }
+
+    private int requestNumberInput(Supplier<Integer> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return requestNumberInput(supplier);
+        }
     }
 }
