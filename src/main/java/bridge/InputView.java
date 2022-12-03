bd55d5423ca71645517a33a28df84630a70d3938
ChaCha3088 <cha3088@gmail.com>
2022-12-03 10:56:15 +0900

    feat(/src/main/java/bridge): add 다리의 길이를 숫자로 입력받는다.

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import static java.lang.Integer.parseInt;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,11 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        Integer input = parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+        if (input < 3 | input > 20) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
+        }
+        return input;
     }
 
     /**
