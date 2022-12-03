0bcd4fb6751b25552950ad144b8ff710efd35667
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 15:09:03 +0900

    feat(OutputPhrase): 게임 시작 문구 추가

--- src/main/java/bridge/view/constant/OutputPhrase.java
+++ src/main/java/bridge/view/constant/OutputPhrase.java
@@ -1,7 +1,8 @@
 package bridge.view.constant;
 
 public enum OutputPhrase {
-    TITLE("최종 게임 결과"),
+    GAME_START("다리 건너기 게임을 시작합니다."),
+    RESULT_TITLE("최종 게임 결과"),
     RESULT("게임 성공 여부: %s"),
     ATTEMPT_COUNT("총 시도한 횟수: %d");
 
