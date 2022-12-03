d6c845c9f7f4f7dabbf70735fad5d8860abb3f62
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 11:26:17 +0900

    feat(Bridge): n번째 칸에 대한 시도 결과 boolean 반환 기능 구현

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import bridge.domain.constant.PanelDirections;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -10,6 +12,15 @@ public class Bridge {
     public Bridge(BridgeLength length, List<String> panels) {
         this.length = length;
         this.panels = panels;
+    }
+
+    public boolean checkPanel(int order, String panelSymbol) {
+        int panelIndex = convertOrderToIndex(order);
+        return panels.get(panelIndex).equals(panelSymbol);
+    }
+
+    private int convertOrderToIndex(int order) {
+        return order - 1;
     }
 
     public int getLength() {
