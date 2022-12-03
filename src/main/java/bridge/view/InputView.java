aa417a74c9c92468c2ccb78d1742cec4aa2c1c54
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 14:13:33 +0900

    feat(view/): 이동할 칸 입력 문구 생성 및 출력

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -23,6 +23,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println(InputPhrase.MOVE.get());
         return null;
     }
 
--- src/main/java/bridge/view/constant/InputPhrase.java
+++ src/main/java/bridge/view/constant/InputPhrase.java
@@ -1,7 +1,9 @@
 package bridge.view.constant;
 
 public enum InputPhrase {
-    BRIDGE_LENGTH("다리의 길이를 입력해주세요.");
+    BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
+    MOVE("이동할 칸을 선택해주세요. (위: " + MoveOptions.UP.get()
+            + ", 아래: " + MoveOptions.DOWN.get() + ")");
 
     private final String phrase;
 
