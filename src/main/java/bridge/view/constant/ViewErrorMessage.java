39c427729acba3668783ec226733122e60ae7d67
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 14:17:48 +0900

    feat(ViewErrorMessage): 올바르지 않은 이동 입력에 대한 에러메세지 추가

--- src/main/java/bridge/view/constant/ViewErrorMessage.java
+++ src/main/java/bridge/view/constant/ViewErrorMessage.java
@@ -2,7 +2,9 @@ package bridge.view.constant;
 
 public enum ViewErrorMessage {
     EMPTY("값을 입력하지 않았습니다."),
-    NOT_NUMBER("숫자만 입력해주세요.");
+    NOT_NUMBER("숫자만 입력해주세요."),
+    INVALID_MOVE_OPTION(MoveOptions.UP.get() + "(위), "
+            + MoveOptions.DOWN.get() + "(아래) 중 하나만 입력해주세요.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
