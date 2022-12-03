028e489607381decc0b59ab85a52e080568bcc82
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 14:47:07 +0900

    feat(ChoiceResult): 스타일 검색 기능 구현

--- src/main/java/bridge/view/constant/ChoiceResultStyle.java
+++ src/main/java/bridge/view/constant/ChoiceResultStyle.java
@@ -1,6 +1,9 @@
 package bridge.view.constant;
 
+import bridge.domain.constant.PanelDirections;
 import bridge.service.constant.ChoiceResult;
+
+import java.util.Arrays;
 
 public enum ChoiceResultStyle {
     CORRECT("O", ChoiceResult.CORRECT),
@@ -13,6 +16,14 @@ public enum ChoiceResultStyle {
     private ChoiceResultStyle(String style, ChoiceResult choiceResult) {
         this.style = style;
         this.choiceResult = choiceResult;
+    }
+
+    public static String findStyleByChoiceResult(ChoiceResult choiceResult) {
+        return Arrays.stream(ChoiceResultStyle.values())
+                .filter(choiceResultStyle -> choiceResult == choiceResultStyle.choiceResult)
+                .findAny()
+                .orElseThrow()
+                .getStyle();
     }
 
     public String getStyle() {
