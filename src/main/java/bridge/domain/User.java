b74e2f9330ce919651364cfbfb9b622ff649b852
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 12:01:30 +0900

    feat(User): 게임 플레이 관련 메서드 구현
    [세부사항]
    - 구현 목록
    1. playRound : 다리의 각 칸 진행 시 호출되는 메서드
    2. fail : 칸에 대한 시도 실패 시 호출되는 메서드
    3. retry : 재시도 할 시 호출되는 메서드
    4. clearGame : 다리를 모두 건넜을 시 호출되는 메서드
    
    - 수정사항
    생성자의 attemptCount 초기값 1로 수정

--- src/main/java/bridge/domain/User.java
+++ src/main/java/bridge/domain/User.java
@@ -10,7 +10,25 @@ public class User {
     public User() {
         this.status = UserStatus.PLAY;
         this.round = 0;
-        this.attemptCount = 0;
+        this.attemptCount = 1;
+    }
+
+    public void playRound() {
+        round++;
+    }
+
+    public void fail() {
+        status = UserStatus.FAIL;
+    }
+
+    public void retry() {
+        round--;
+        attemptCount++;
+        status = UserStatus.PLAY;
+    }
+
+    public void clearGame() {
+        status = UserStatus.SUCCESS;
     }
 
     public UserStatus getStatus() {
