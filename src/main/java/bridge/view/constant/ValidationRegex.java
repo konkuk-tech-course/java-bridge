48ee8d8159906ea0cd3f18dc566686e731b256f6
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 14:14:54 +0900

    feat(ValidationRegex): 이동할 칸 입력 유효성 검증 위한 정규표현식 추가

--- src/main/java/bridge/view/constant/ValidationRegex.java
+++ src/main/java/bridge/view/constant/ValidationRegex.java
@@ -2,7 +2,8 @@ package bridge.view.constant;
 
 public enum ValidationRegex {
     EMPTY("^$"),
-    ONLY_NUMBER("^[0-9]+$");
+    ONLY_NUMBER("^[0-9]+$"),
+    MOVE_OPTION("^[UD]+$");
 
     private final String regex;
 
