bfc73c3e0046cd63eeec528a34b7f0dec30806b7
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 15:22:45 +0900

    feat(GameController): 게임 진행 컨트롤러 기능 구현

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -1,14 +1,19 @@
 package bridge.controller;
 
 import bridge.domain.Bridge;
+import bridge.domain.User;
+import bridge.domain.constant.UserStatus;
+import bridge.service.BridgeGame;
 import bridge.service.BridgeService;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+import bridge.view.constant.RetryOptions;
 
 import java.util.function.Supplier;
 
 public class GameController {
     private static BridgeService bridgeService = BridgeService.getInstance();
+    private static BridgeGame bridgeGame = BridgeGame.getInstance();
     private static InputView inputView = InputView.getInstance();
     private static OutputView outputView = OutputView.getInstance();
 
@@ -31,6 +36,32 @@ public class GameController {
         } catch (IllegalArgumentException e) {
             outputView.printErrorMessage(e.getMessage());
             return requestNumberInput(supplier);
+        }
+    }
+
+    public void operate(User user, Bridge bridge) {
+        while(user.getStatus() == UserStatus.PLAY) {
+            String option = requestOptionInput(inputView::readMoving);
+            bridgeGame.move(user, bridge, option);
+            if (user.getStatus() == UserStatus.FAIL) {
+                retry(user);
+            }
+        }
+    }
+
+    private void retry(User user) {
+        String option = requestOptionInput(inputView::readGameCommand);
+        if (option.equals(RetryOptions.RETRY.get())) {
+            bridgeGame.retry(user);
+        }
+    }
+
+    private String requestOptionInput(Supplier<String> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return requestOptionInput(supplier);
         }
     }
 }
