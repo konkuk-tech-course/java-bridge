44fafd1243dde92540da3b680d5b89f83cf97bd1
ChaCha3088 <cha3088@gmail.com>
2022-12-03 11:10:56 +0900

    feat(/src/main/java/bridge): add 출력 추가

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -4,14 +4,20 @@ import java.util.List;
 
 public class BridgeGameController {
     public static List<String> initializeBridge(Integer bridgeSize) {
+        OutputView.String("다리 건너기 게임을 시작합니다.");
         BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
         return bridgeMaker.makeBridge(bridgeSize);
     }
 
+    public static void play() {
+        
+    }
+
     public static Integer repeatReadBridgeSize() {
         while (true) {
             try {
+                OutputView.String("다리의 길이를 입력해주세요.");
                 Integer bridgeSize = InputView.readBridgeSize();
                 return bridgeSize;
             } catch (NumberFormatException e) {
