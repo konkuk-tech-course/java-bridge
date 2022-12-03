ff33c50d1516f9aa37fb61ba417e6239893a4620
skydreamer21 <skydreamer210@gmail.com>
2022-12-03 10:41:57 +0900

    feaet(BridgeLength): 필드, 생성자, getter 구현

---
+++ src/main/java/bridge/domain/BridgeLength.java
@@ -0,0 +1,13 @@
+package bridge.domain;
+
+public class BridgeLength {
+    private int length;
+
+    public BridgeLength(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
