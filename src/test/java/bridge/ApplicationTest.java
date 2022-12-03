b11351779784bedc179b85d8a3a9ed6067a36d1b
penrose15 <seongju99@naver.com>
2022-12-03 14:52:29 +0900

    test : add testcase

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -98,6 +98,28 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 실패_실패_성공_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            run("3", "U", "D", "D","R","U","U","R","U","D","U");
+            assertThat(output()).contains(
+                    "[ O |   |   ]",
+                    "[   | O | X ]",
+                    "[ O | X ]",
+                    "[   |   ]",
+                    "최종 게임 결과",
+                    "[ O |   | O ]",
+                    "[   | O |   ]",
+                    "게임 성공 여부: 성공",
+                    "총 시도한 횟수: 3"
+            );
+
+            int upSideIndex = output().indexOf("[ O |   | O ]");
+            int downSideIndex = output().indexOf("[   | O |   ]");
+            assertThat(upSideIndex).isLessThan(downSideIndex);
+        }, 1, 0, 1);
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("a");
