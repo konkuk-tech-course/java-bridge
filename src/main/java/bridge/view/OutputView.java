5cc82751c9112be9782a056909f542ae3208f467
juno-junho <ssmm0205@naver.com>
2022-12-04 12:40:05 +0900

    refactor: enum을 이용한 리팩터링

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.constraints.OutputMessage;
+
+import static bridge.constraints.OutputMessage.*;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -23,6 +27,7 @@ public class OutputView {
     }
 
     public void printStartMessage() {
-        System.out.println("다리 건너기 게임을 시작합니다.");
+        String message = START_MESSAGE.getMessage();
+        System.out.println(message);
     }
 }
