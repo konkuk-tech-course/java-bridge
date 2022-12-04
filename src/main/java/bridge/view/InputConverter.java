42324eccc23d7489f36ad20a842ae8867d58380e
juno-junho <ssmm0205@naver.com>
2022-12-04 12:59:31 +0900

    refactor: boolean 메서드 분리

--- src/main/java/bridge/view/InputConverter.java
+++ src/main/java/bridge/view/InputConverter.java
@@ -5,12 +5,20 @@ import static bridge.constraints.MoveCode.*;
 public class InputConverter {
 
     public static String convertToCode(Integer i) {
-        if (i == UPSIGN.getIntegerCode()){
+        if (isUpSign(i)){
             return UPSIGN.getStringCode();
         }
-        if (i == DOWNSIGN.getIntegerCode()) {
+        if (isDownSign(i)) {
             return DOWNSIGN.getStringCode();
         }
         throw new IllegalArgumentException("[ERROR] Input을 변환 할 수 없습니다.");
+    }
+
+    private static boolean isDownSign(Integer i) {
+        return i == DOWNSIGN.getIntegerCode();
+    }
+
+    private static boolean isUpSign(Integer i) {
+        return i == UPSIGN.getIntegerCode();
     }
 }
