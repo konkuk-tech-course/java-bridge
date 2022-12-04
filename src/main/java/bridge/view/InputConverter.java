cc8c42e65302e505f4ab9bcaf5adff31a60c5444
juno-junho <ssmm0205@naver.com>
2022-12-04 12:16:07 +0900

    refactor: 매직 넘버를 enum으로 변경

--- src/main/java/bridge/view/InputConverter.java
+++ src/main/java/bridge/view/InputConverter.java
@@ -1,13 +1,15 @@
 package bridge.view;
 
+import static bridge.constraints.MoveCode.*;
+
 public class InputConverter {
 
     public static String convertToCode(Integer i) {
-        if (i == 1){
-            return "U";
+        if (i == UPSIGN.getIntegerCode()){
+            return UPSIGN.getStringCode();
         }
-        if (i == 0) {
-            return "D";
+        if (i == DOWNSIGN.getIntegerCode()) {
+            return DOWNSIGN.getStringCode();
         }
         throw new IllegalArgumentException("[ERROR] Input을 변환 할 수 없습니다.");
     }
