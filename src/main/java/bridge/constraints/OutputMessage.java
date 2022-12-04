1d91d450a05bca32541535313e7f3ef669fe86b6
juno-junho <ssmm0205@naver.com>
2022-12-04 12:39:41 +0900

    refactor: rename OutputMessage enum field

--- src/main/java/bridge/constraints/OutputMessage.java
+++ src/main/java/bridge/constraints/OutputMessage.java
@@ -2,14 +2,14 @@ package bridge.constraints;
 
 public enum OutputMessage {
 
-    FINAL_GAME_RESULT_MESSAGE("최종 게임 결과"),
-    RETRY_SUM_MESSAGE("총 시도한 횟수: "),
-    SUCCESS_MESSAGE("게임 성공 여부: 성공"),
-    FAIL_MESSAGE("게임 성공 여부: 실패"),
     START_MESSAGE("다리 건너기 게임을 시작합니다."),
-    BAR(" | "),
-    LEFT_BOARDER("[ "),
-    RIGHT_BOARDER(" ]");
+    FINAL_RESULT_MESSAGE("최종 게임 결과"),
+    SUCCESS_MESSAGE("게임 성공 여부: 성공"),
+    FAILURE_MESSAGE("게임 성공 여부: 실패"),
+    TRY_COUNT_MESSAGE("총 시도한 횟수: "),
+    LEFT_SQARE_BRACKET("["),
+    RIGHT_SQARE_BRACKET("]"),
+    BAR("|");
 
     private final String message;
 
