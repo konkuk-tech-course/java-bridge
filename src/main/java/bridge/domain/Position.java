4a67d492f67852db7950ef30fc58e06321595d85
juno-junho <ssmm0205@naver.com>
2022-12-04 13:28:22 +0900

    feat: Position 클래스 생성을 통한 위치 추적

--- src/main/java/bridge/domain/Position.java
+++ src/main/java/bridge/domain/Position.java
@@ -1,6 +1,35 @@
 package bridge.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 public class Position {
 
-    private static final List<I>
+    private static final Map<Integer, Position> POSITIONS = new HashMap<>();
+
+    static {
+        IntStream.range(0, 20)
+                .forEach(i -> POSITIONS.put(i, new Position(i)));
+    }
+    private final int value;
+    public Position(int value) {
+        this.value = value;
+    }
+
+    public Position of(int key) {
+        validatekey(key);
+        return POSITIONS.get(key);
+    }
+
+    private void validatekey(int key) {
+        if (!POSITIONS.containsKey(key)) {
+            throw new IllegalArgumentException("[ERROR] 플레이어 위치값은 0에서 19까지만 가능합니다");
+        }
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
 }
