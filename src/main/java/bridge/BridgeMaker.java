007d6cc54c45087b6308d013565048d42c60b671
juno-junho <ssmm0205@naver.com>
2022-12-04 12:08:26 +0900

    feat: 다리를 생성하는 메서드 생성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,12 @@
 package bridge;
 
+import bridge.view.InputConverter;
+import bridge.view.InputView;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +24,10 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return IntStream.range(0,size)
+                .map(i -> bridgeNumberGenerator.generate())
+                .mapToObj(i -> InputConverter.convertToCode(i))
+                .collect(Collectors.toList());
     }
+
 }
