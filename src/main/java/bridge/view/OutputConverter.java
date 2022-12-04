f15a0bb13dbed06ce8e034f5e2c954e638413420
juno-junho <ssmm0205@naver.com>
2022-12-04 12:40:49 +0900

    feat: bridge를 ui로 변경하는 클래스 생성

---
+++ src/main/java/bridge/view/OutputConverter.java
@@ -0,0 +1,20 @@
+package bridge.view;
+
+import bridge.BridgeMaker;
+
+import java.util.List;
+
+public class OutputConverter {
+    private final BridgeMaker bridgeMaker;
+
+    public OutputConverter(BridgeMaker bridgeMaker) {
+        this.bridgeMaker = bridgeMaker;
+    }
+
+    public String convertbridgeToMessage(List<String> bridge) {
+
+
+    }
+
+
+}
